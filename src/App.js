2ca695ee929ce6e6efbc53ce9f2659c162508299
pak11273 <pak11273@gmail.com>
2021-02-16 01:02:38 -0600

    adds handleclick to all caclbuttons

--- src/App.js
+++ src/App.js
@@ -44,20 +44,20 @@ function App() {
 
             <div className="row">
               <CalcButton value={1} onClick={() => handleClick(1)} />
-              <CalcButton value={2} />
-              <CalcButton value={3} />
+              <CalcButton value={2} onClick={() => handleClick(2)} />
+              <CalcButton value={3} onClick={() => handleClick(3)} />
             </div>
 
             <div className="row">
-              <CalcButton value={4} />
-              <CalcButton value={5} />
-              <CalcButton value={6} />
+              <CalcButton value={4} onClick={() => handleClick(4)} />
+              <CalcButton value={5} onClick={() => handleClick(5)} />
+              <CalcButton value={6} onClick={() => handleClick(6)} />
             </div>
 
             <div className="row">
-              <CalcButton value={7} />
-              <CalcButton value={8} />
-              <CalcButton value={9} />
+              <CalcButton value={7} onClick={() => handleClick(7)} />
+              <CalcButton value={8} onClick={() => handleClick(8)} />
+              <CalcButton value={9} onClick={() => handleClick(9)} />
             </div>
 
             <div className="row">
