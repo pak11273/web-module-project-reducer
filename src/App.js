d34772364261cc0ea14b79ba6e1cd8b7f2fe8b93
pak11273 <pak11273@gmail.com>
2021-02-16 01:12:56 -0600

    add clear functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 import React, { useReducer } from "react";
-import { applyNumber, changeOperation } from "./actions";
+import { applyNumber, changeOperation, clearDisplay } from "./actions";
 import reducer, { initialState } from "./reducers";
 
 import CalcButton from "./components/CalcButton";
@@ -15,6 +15,10 @@ function App() {
   };
   const handleOperation = (operation) => {
     dispatch(changeOperation(operation));
+  };
+
+  const handleClearDisplay = () => {
+    dispatch(clearDisplay());
   };
   return (
     <div className="App">
@@ -70,7 +74,7 @@ function App() {
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"} />
+              <CalcButton value={"CE"} onClick={() => handleClearDisplay()} />
             </div>
           </form>
         </div>
