b518865387558c62a4be0a2bf834f717bd9ad6bd
pak11273 <pak11273@gmail.com>
2021-02-16 00:50:32 -0600

    complete one button

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,12 @@
 # Understanding Questions:
-1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
-* The user presses the 1 button.
-* 
-...
 
-* TotalDisplay shows the total plus 1.
+1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
+
+- The user presses the 1 button.
+- The onclick handler is invoked.
+- The dispatch function is invoked.
+- The addOne function is invoked.
+- The reducer function is invoked.
+- The reducer function receives the action from the addOne function and state from App.js
+- The reducer function returns a new state which updates the state in App.js.
+- TotalDisplay shows the total plus 1.
--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import "./App.css";
 import React, { useReducer } from "react";
 import reducer, { initialState } from "./reducers";
 
-import { ADD_ONE } from "./actions";
 import CalcButton from "./components/CalcButton";
 import TotalDisplay from "./components/TotalDisplay";
+import { addOne } from "./actions";
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const handleClick = () => {
+    dispatch(addOne());
+  };
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="#">
-          <img width="40px" src="./Lambda-Logo-Red.png" /> Lambda Reducer
-          Challenge
+        <a className="navbar-brand" href="/">
+          <img width="40px" src="./Lambda-Logo-Red.png" alt="something" />{" "}
+          Lambda Reducer Challenge
         </a>
       </nav>
 
@@ -39,7 +42,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1} />
+              <CalcButton value={1} onClick={handleClick} />
               <CalcButton value={2} />
               <CalcButton value={3} />
             </div>
